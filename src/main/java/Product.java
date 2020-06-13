a06a7a046d6b6338994e948b250759f7e4a9275b
Hubert Macias <s21163@pjwstk.edu.pl>
2020-06-13 14:54:01 +0200

    czesc zadania 2

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,7 +1,11 @@
+import javax.validation.constraints.NotNull;
+
 public class Product {
 
     private String productId;
     private String productName;
+
+    @NotNull(message = "Price cannot be null")
     private double price;
 
 
--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,8 +1,11 @@
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 
 public class Shop {
 
+    @Size(min = 6, max = 500, message = "Shop name must have at least 6 characters")
     private String shopName;
+
     private String shopId;
 
     ArrayList<String> availableProducts = new ArrayList<String>();
